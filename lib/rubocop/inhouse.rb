e68ec25a56ca74c3c4f02edb301c993865feda52
Nolan J Tait <nolanjtait@gmail.com>
2023-10-28 13:33:13 -0700

    Adds project root and binstubs

--- lib/rubocop/inhouse.rb
+++ lib/rubocop/inhouse.rb
@@ -4,7 +4,8 @@ require_relative "inhouse/version"
 
 module Rubocop
   module Inhouse
-    class Error < StandardError; end
-    # Your code goes here...
+    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
+    CONFIG_DEFAULT = PROJECT_ROOT.join("config", "default.yml").freeze
+    CONFIG_RAILS = PROJECT_ROOT.join("config", "rails.yml").freeze 
   end
 end
