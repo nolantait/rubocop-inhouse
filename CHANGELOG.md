19fe41b71c644f23b048872f2a45f82335683aa2
Nolan J Tait <nolanjtait@gmail.com>
2024-03-05 23:39:55 -0800

    Bumps gem version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## [0.1.0] - 2023-10-28
 
 - Initial release
+
+## [0.1.3] - 2024-03-05
+
+- Removes Style/ConstantVisibility, too verbose
--- lib/rubocop/inhouse/version.rb
+++ lib/rubocop/inhouse/version.rb
@@ -2,7 +2,7 @@
 
 module Rubocop
   module Inhouse
-    VERSION = "0.1.2"
+    VERSION = "0.1.3"
     public_constant :VERSION
   end
 end
